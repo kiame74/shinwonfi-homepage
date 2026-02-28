9fb39c14947c17a8de1ceab62e1f06d2dcf2771c
kiame74 <kiame74@github.com>
2026-02-28 17:26:14 +0900

    fix: resolve all build warnings and finalize package.json

--- src/app/(public)/support/notice/page.tsx
+++ src/app/(public)/support/notice/page.tsx
@@ -39,6 +39,7 @@ export default function NoticePage() {
         }
     };
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         const timer = setTimeout(() => {
             fetchNotices();
